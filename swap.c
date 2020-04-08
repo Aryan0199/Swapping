6814a3b0a09f7233622b81adf01af66c62820f9e
Aryan0199 <63339536+Aryan0199@users.noreply.github.com>
2020-04-08 15:23:18 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -7,8 +7,9 @@ int main() {
     scanf("%d",&a);
     scanf("%d",&b);
     printf(" Initially---> a=%d and b=%d\n",a,b);
-    a=a+b-a;
-    b=a+b-b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
     printf(" After swapping---> a=%d and b=%d",a,b);
     return 0;
 }
